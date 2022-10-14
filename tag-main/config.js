c685fa67e1cde31156e80a071e8fc9898aa79e50
MinhDuc28 <85271317+MinhDuc28@users.noreply.github.com>
2022-10-14 23:21:21 +0700

    Add files via upload

---
+++ tag-main/config.js
@@ -0,0 +1,17 @@
+const CONFIG = {
+    introTitle: 'Hi Ngân Trần',
+    introDesc: `Chào mừng em đã
+   đến với ngày kỉ niệm siêu khủng này`,
+    btnIntro: 'Vào đê',
+    title: 'Nhìn cái này quen hok cưng  🥰',
+    desc: 'Phải chăng anh đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Quen ạ , anh Đức iu của em 😘',
+    btnNo: 'ĐÉO QUEN',
+    question:'Gửi ảnh nú dè để tiếp tục',
+    btnReply: 'Gửi cho anh đê',
+    reply: 'Yêu thì yêu mà không yêu thì yêu cmn đi',
+    mess: 'Anh biết mà mnem mnem * ực ực 😘😘',
+    messDesc: 'Thế vẫn chỗ cũ nhá',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://youtu.be/pqyBt0PbYJ0' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
